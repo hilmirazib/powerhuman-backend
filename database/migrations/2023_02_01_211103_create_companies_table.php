f5bfc39929e785f8fbac70ceb4226f314a8b001d
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2023-02-02 09:06:37 +0700

    migration

--- database/migrations/2023_02_01_211103_create_companies_table.php
+++ database/migrations/2023_02_01_211103_create_companies_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('logo')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
